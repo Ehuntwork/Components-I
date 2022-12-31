7337aaa3d3f935b0c0b4a732b2e4c825b5646146
Ehuntwork <sir.mle.learnsalot@gmail.com>
2022-12-30 17:36:01 -0800

    articles created and appended

--- src/components/article/article.js
+++ src/components/article/article.js
@@ -87,6 +87,27 @@ const data = [
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
+  },
+  {
+    title: 'NEEEEEEWWWWW AAARTICLE"',
+    date: 'Nov 5th, 2018',
+    firstParagraph: `Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura skywalker lando
+        moff wicket tatooine luke.Solo wampa wampa calrissian yoda moff.Darth grievous darth gonk darth hutt.Darth baba skywalker
+        watto fett jango maul han.Mon ewok sidious sidious lando kenobi grievous gamorrean solo.Yoda wedge utapau darth calamari.
+        Hutt calamari darth jabba.Darth dooku amidala organa moff.Boba darth binks solo hutt skywalker dantooine skywalker.Qui - gonn
+        jar twi'lek jinn leia jango skywalker mon.`,
+
+    secondParagraph: `Grievous fett calamari anakin skywalker hutt.Alderaan darth kenobi darth r2- d2
+        windu mothma.Sidious darth calamari moff.Wampa mothma sith wedge solo mara.Darth gonk maul sith moff chewbacca palpatine
+        mace amidala.C - 3po solo skywalker anakin yoda leia.Maul wampa bespin watto jade ewok darth jabba.Lando dantooine moff
+        k - 3po dantooine luke.Fisto mandalore darth wedge c - 3p0 ahsoka.Secura moff palpatine fett.Anakin sith darth darth.Moff
+        solo leia ben ponda jade.Binks jango aayla skywalker skywalker cade.Mustafar darth ventress anakin watto.Yavin jawa sebulba
+        owen jinn tatooine sith organa.`,
+
+    thirdParagraph: `Dagobah hutt jawa leia calamari ventress skywalker yoda. Binks wicket hutt coruscant sidious
+        naboo ackbar tatooine. Hutt lars padmé darth. Maul solo darth darth jabba qui-gon chewbacca darth maul. Moff baba wicket
+        han. C-3po antilles moff qui-gon ahsoka aayla dooku amidala. Palpatine droid amidala droid k-3po twi'lek padmé wookiee. Leia
+        moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.`
   }
 ];
 
@@ -115,3 +136,54 @@ const data = [
   Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
   Refresh the page to see the new article.
 */
+
+let articleMaker = function(obj){
+  //create elements
+  let articleConatiner = document.createElement('div')
+  let articleTitle = document.createElement('h2')
+  let articleDate = document.createElement('p')
+  let articleParag1 = document.createElement('p')
+  let articleParag2 = document.createElement('p')
+  let articleParag3 = document.createElement('p') 
+  let articleSpanButton = document.createElement('span')
+
+  let appendElements = [
+    articleTitle, 
+    articleDate,
+    articleParag1,
+    articleParag2,
+    articleParag3,
+    articleSpanButton]
+
+  //assign classes
+  articleConatiner.classList.add('article')
+  articleDate.classList.add('date')
+  articleSpanButton.classList.add('expandButton')
+
+  //add textcontent
+  articleTitle.textContent = obj.title
+  articleDate.textContent = obj.date
+  articleParag1.textContent = obj.firstParagraph
+  articleParag2.textContent = obj.secondParagraph
+  articleParag3.textContent = obj.thirdParagraph
+  articleSpanButton.textContent = '+'
+
+  //add event listener
+  articleSpanButton.addEventListener('click', evt=>{
+    articleConatiner.classList.toggle('article-open')
+  })
+
+  //append to container div
+    appendElements.forEach(elem =>{
+      articleConatiner.appendChild(elem)
+    })
+
+  return articleConatiner
+}
+
+//add to dom using data
+let articles = document.querySelector('.articles')
+
+data.forEach(elem =>{
+  articles.appendChild(articleMaker(elem))
+})
